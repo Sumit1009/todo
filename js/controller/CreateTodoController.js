bf5a0f8d220adf48f30c083885af24bec177f6ea
Roni Thomas <roni.thomas@tothenew.com>
2017-06-22 17:46:38 +0530

    validations added

--- js/controller/CreateTodoController.js
+++ js/controller/CreateTodoController.js
@@ -2,13 +2,16 @@ app.controller('CreateTodo',["$scope","TodoService",function ($scope,TodoService
     $scope.todo = {};
     // $scope.todos=[];
     $scope.submit = function () {
-        var newTodo = {}
-        console.log($scope.todo);
-        angular.copy($scope.todo, newTodo)
-        TodoService.add(newTodo)
-        console.log($scope.todos)
+
+            var newTodo = {};
+            console.log($scope.todo);
+            angular.copy($scope.todo, newTodo);
+            TodoService.add(newTodo);
+            console.log($scope.todos);
     };
 
     $scope.todos=TodoService.getTodos();
+
+    $scope.minDate=new Date();
 
 }]);
