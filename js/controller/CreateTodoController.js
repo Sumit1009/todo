525675ecb1e201ef0099e69a3936bf024b8d7d9a
jitender saini <jitender.saini@tothenew.com>
2017-06-22 17:08:26 +0530

    todo list displayed and deleted working

--- js/controller/CreateTodoController.js
+++ js/controller/CreateTodoController.js
@@ -1,7 +1,24 @@
-app.controller('CreateTodo',["$scope","todoService",function ($scope) {
+app.controller('CreateTodo',["$scope",function ($scope) {
     $scope.todo = {};
-    $scope.todos=[]
+    $scope.todos=[
+        {
+            task: 'hello1',
+            date: "12/11/2015"
+        },
+        {
+            task: 'hello2',
+            date: "12/11/2015"
+        },
+        {
+            task: 'hello3',
+            date: "12/11/2015"
+        }
+    ];
     $scope.submit = function () {
         $scope.todos.push(todo)
-    }
+    };
+    $scope.remove = function(todo) {
+        var index = $scope.todos.indexOf(todo);
+        $scope.todos.splice(index, 1);
+    };
 }]);
